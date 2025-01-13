6a3a73fe941bf190b5ed44c41b621a566f28ac8d
“Dora-Suke-Ryo” <fujipon.rock@gmail.com>
2025-01-14 02:06:59 +0900

    5th commit

--- src/main/java/com/numbergame/number_game/controller/GameController.java
+++ src/main/java/com/numbergame/number_game/controller/GameController.java
@@ -1,10 +1,31 @@
 package com.numbergame.number_game.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.numbergame.number_game.service.GameService;
+
+@Controller
 public class GameController {
 
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+	@Autowired
+	private GameService gameService;
 
+	@GetMapping("/")
+	public String gamePage (Model model) {
+		gameService.startGame();
+		model.addAttribute("message" , "1から100の間の数字を予想してください");
+		return "game";
 	}
-
+	
+	@PostMapping("/guess")
+	public String checkGuess(@RequestParam("guess") int userGuess, Model model) {
+		String result = gameService.guessNumber(userGuess);
+        model.addAttribute("message", result);
+        return "game";
+	}
 }
--- src/main/java/com/numbergame/number_game/service/GameService.java
+++ src/main/java/com/numbergame/number_game/service/GameService.java
@@ -1,10 +1,29 @@
 package com.numbergame.number_game.service;
 
+import java.util.Random;
+
+import org.springframework.stereotype.Service;
+
+@Service
 public class GameService {
-
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
+	private int targetNumber;
+	private int numberOfTries;
+	
+	public void startGame() {
+		Random rand = new Random();
+		targetNumber = rand.nextInt(100) + 1;
+		numberOfTries = 0;
 	}
+	
+	public String guessNumber(int userGuess) {
+        numberOfTries++;
+        if (userGuess < targetNumber) {
+            return "もっと大きい数字を試してください！";
+        } else if (userGuess > targetNumber) {
+            return "もっと小さい数字を試してください！";
+        } else {
+            return "おめでとうございます！正解は " + targetNumber + " でした。試行回数: " + numberOfTries;
+        }
+    }
 
 }
