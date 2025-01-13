298f24620367516eff2fbf8013fcaa845ca36354
“Dora-Suke-Ryo” <fujipon.rock@gmail.com>
2025-01-13 16:44:55 +0900

    1st commit

---
+++ src/test/java/com/numbergame/number_game/NumberGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.numbergame.number_game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumberGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
